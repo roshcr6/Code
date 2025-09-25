956495ff0555c158f244ec3d3a00a0343a765b1b
Roshith Robert <135985741+roshcr6@users.noreply.github.com>
2025-09-25 22:53:48 +0530

    26_sept

---
+++ C/Day-7/bs.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) return createNode(data);
+    if (data < root->data)
+        root->left = insert(root->left, data);
+    else if (data > root->data)
+        root->right = insert(root->right, data);
+    return root;
+}
+
+struct Node* minValueNode(struct Node* node) {
+    struct Node* current = node;
+    while (current && current->left != NULL)
+        current = current->left;
+    return current;
+}
+
+struct Node* deleteNode(struct Node* root, int key) {
+    if (root == NULL) return root;
+
+    if (key < root->data)
+        root->left = deleteNode(root->left, key);
+    else if (key > root->data)
+        root->right = deleteNode(root->right, key);
+    else {
+        if (root->left == NULL) {
+            struct Node* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+        struct Node* temp = minValueNode(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void preorder(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct Node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+struct Node* search(struct Node* root, int key) {
+    if (root == NULL || root->data == key)
+        return root;
+    if (key < root->data)
+        return search(root->left, key);
+    return search(root->right, key);
+}
+
+int main() {
+    struct Node* root = NULL;
+    int choice, data;
+
+    while (1) {
+        printf("\n=== BST MENU ===\n");
+        printf("1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Inorder Traversal\n");
+        printf("4. Preorder Traversal\n");
+        printf("5. Postorder Traversal\n");
+        printf("6. Search\n");
+        printf("7. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+        case 1:
+            printf("Enter value to insert: ");
+            scanf("%d", &data);
+            root = insert(root, data);
+            break;
+        case 2:
+            printf("Enter value to delete: ");
+            scanf("%d", &data);
+            root = deleteNode(root, data);
+            break;
+        case 3:
+            printf("Inorder Traversal: ");
+            inorder(root);
+            printf("\n");
+            break;
+        case 4:
+            printf("Preorder Traversal: ");
+            preorder(root);
+            printf("\n");
+            break;
+        case 5:
+            printf("Postorder Traversal: ");
+            postorder(root);
+            printf("\n");
+            break;
+        case 6:
+            printf("Enter value to search: ");
+            scanf("%d", &data);
+            if (search(root, data) != NULL)
+                printf("Node %d found in the BST.\n", data);
+            else
+                printf("Node %d not found.\n", data);
+            break;
+        case 7:
+            printf("Exiting...\n");
+            exit(0);
+        default:
+            printf("Invalid choice!\n");
+        }
+    }
+    return 0;
+}
