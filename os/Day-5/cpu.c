416719ed9896df46c2f667f5433052c1d1fb66d6
Roshith Robert <135985741+roshcr6@users.noreply.github.com>
2026-01-30 10:55:25 +0530

    updated

--- os/Day-5/cpu.c
+++ os/Day-5/cpu.c
@@ -1,158 +1,226 @@
 #include <stdio.h>
 
-int AT[10], BT[10], PR[10];
-int CT[10], TAT[10], WT[10];
-int n;
+struct process {
+    int pid, at, bt, pr;
+    int ct, tat, wt;
+};
 
-/* Common function to print table */
-void printTable() {
-    float avgTAT = 0, avgWT = 0;
-
-    printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\n");
-    for(int i = 0; i < n; i++) {
-        TAT[i] = CT[i] - AT[i];
-        WT[i]  = TAT[i] - BT[i];
-        avgTAT += TAT[i];
-        avgWT  += WT[i];
-
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
-               i+1, AT[i], BT[i], CT[i], TAT[i], WT[i]);
-    }
-
-    printf("Average Turnaround Time = %.2f\n", avgTAT / n);
-    printf("Average Waiting Time    = %.2f\n", avgWT / n);
+void printGantt(int gp[], int gt[], int gc) {
+    printf("\nGantt Chart:\n|");
+    for (int i = 0; i < gc; i++)
+        printf(" P%d |", gp[i]);
+    printf("\n");
+    for (int i = 0; i < gc; i++)
+        printf("%-5d", gt[i]);
+    printf("%d\n", gt[gc]);
 }
 
-/* FCFS Scheduling */
-void fcfs() {
-    int time = 0;
+void fcfs(struct process p[], int n) {
+    int time = 0, gp[50], gt[50], gc = 0;
 
-    for(int i = 0; i < n; i++) {
-        if(time < AT[i])
-            time = AT[i];
-        time += BT[i];
-        CT[i] = time;
+    for(int i = 0; i < n; i++)
+        p[i].ct = p[i].tat = p[i].wt = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (time < p[i].at)
+            time = p[i].at;
+
+        gp[gc] = p[i].pid;
+        gt[gc++] = time;
+
+        time += p[i].bt;
+        p[i].ct = time;
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
     }
-    printTable();
+    gt[gc] = time;
+
+    printGantt(gp, gt, gc);
+
+    printf("\nPID AT BT CT TAT WT\n");
+    for (int i = 0; i < n; i++)
+        printf("P%d  %d  %d  %d  %d  %d\n",
+               p[i].pid, p[i].at, p[i].bt,
+               p[i].ct, p[i].tat, p[i].wt);
 }
 
-/* SJF (Non-Preemptive) */
-void sjf() {
-    int done[10] = {0};
+void sjf(struct process p[], int n) {
     int time = 0, completed = 0;
+    int done[n], gp[50], gt[50], gc = 0;
 
-    while(completed < n) {
-        int idx = -1, minBT = 9999;
+    for (int i = 0; i < n; i++) {
+        done[i] = 0;
+        p[i].ct = p[i].tat = p[i].wt = 0;
+    }
 
-        for(int i = 0; i < n; i++) {
-            if(AT[i] <= time && !done[i] && BT[i] < minBT) {
-                minBT = BT[i];
+    while (completed < n) {
+        int idx = -1, min = 9999;
+
+        for (int i = 0; i < n; i++) {
+            if (!done[i] && p[i].at <= time && p[i].bt < min) {
+                min = p[i].bt;
                 idx = i;
             }
         }
 
-        if(idx == -1) {
+        if (idx == -1) {
             time++;
             continue;
         }
 
-        time += BT[idx];
-        CT[idx] = time;
+        gp[gc] = p[idx].pid;
+        gt[gc++] = time;
+
+        time += p[idx].bt;
+        p[idx].ct = time;
+        p[idx].tat = p[idx].ct - p[idx].at;
+        p[idx].wt = p[idx].tat - p[idx].bt;
+
         done[idx] = 1;
         completed++;
     }
-    printTable();
+    gt[gc] = time;
+
+    printGantt(gp, gt, gc);
+
+    printf("\nPID AT BT CT TAT WT\n");
+    for (int i = 0; i < n; i++)
+        printf("P%d  %d  %d  %d  %d  %d\n",
+               p[i].pid, p[i].at, p[i].bt,
+               p[i].ct, p[i].tat, p[i].wt);
 }
 
-/* Priority Scheduling (Non-Preemptive) */
-void priority() {
-    int done[10] = {0};
-    int time = 0, completed = 0;
+void roundRobin(struct process p[], int n) {
+    int tq, rt[n], time = 0, completed = 0;
+    int gp[50], gt[50], gc = 0;
 
-    while(completed < n) {
-        int idx = -1, minPR = 9999;
-
-        for(int i = 0; i < n; i++) {
-            if(AT[i] <= time && !done[i] && PR[i] < minPR) {
-                minPR = PR[i];
-                idx = i;
-            }
-        }
-
-        if(idx == -1) {
-            time++;
-            continue;
-        }
-
-        time += BT[idx];
-        CT[idx] = time;
-        done[idx] = 1;
-        completed++;
+    for (int i = 0; i < n; i++) {
+        rt[i] = p[i].bt;
+        p[i].ct = p[i].tat = p[i].wt = 0;
     }
-    printTable();
-}
-
-/* Round Robin Scheduling */
-void roundRobin() {
-    int tq, rem[10];
-    int time = 0, completed = 0;
 
     printf("Enter Time Quantum: ");
     scanf("%d", &tq);
 
-    for(int i = 0; i < n; i++)
-        rem[i] = BT[i];
+    while (completed < n) {
+        int executed = 0;
 
-    while(completed < n) {
-        for(int i = 0; i < n; i++) {
-            if(rem[i] > 0 && AT[i] <= time) {
-                if(rem[i] > tq) {
+        for (int i = 0; i < n; i++) {
+            if (rt[i] > 0 && p[i].at <= time) {
+                executed = 1;
+
+                gp[gc] = p[i].pid;
+                gt[gc++] = time;
+
+                if (rt[i] > tq) {
                     time += tq;
-                    rem[i] -= tq;
+                    rt[i] -= tq;
                 } else {
-                    time += rem[i];
-                    rem[i] = 0;
-                    CT[i] = time;
+                    time += rt[i];
+                    rt[i] = 0;
+                    p[i].ct = time;
                     completed++;
                 }
             }
         }
+        if (!executed)
+            time++;
     }
-    printTable();
+    gt[gc] = time;
+
+    printGantt(gp, gt, gc);
+
+    for (int i = 0; i < n; i++) {
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
+    }
+
+    printf("\nPID AT BT CT TAT WT\n");
+    for (int i = 0; i < n; i++)
+        printf("P%d  %d  %d  %d  %d  %d\n",
+               p[i].pid, p[i].at, p[i].bt,
+               p[i].ct, p[i].tat, p[i].wt);
 }
 
-int main() {
-    int choice;
+void priority(struct process p[], int n) {
+    int time = 0, completed = 0;
+    int done[n], gp[50], gt[50], gc = 0;
 
-    do {
-        printf("\n--- CPU Scheduling Menu ---\n1. FCFS\n2. SJF (Non-Preemptive)\n3. Round Robin (Pre-emptive)\n4. Priority (Non-Preemptive)\n5. Exit\nEnter your choice: ");
-        scanf("%d", &choice);
+    for (int i = 0; i < n; i++) {
+        done[i] = 0;
+        p[i].ct = p[i].tat = p[i].wt = 0;
+    }
 
-        if(choice >= 1 && choice <= 4) {
-            printf("Enter number of processes: ");
-            scanf("%d", &n);
+    while (completed < n) {
+        int idx = -1, high = 9999;
 
-            for(int i = 0; i < n; i++) {
-                if(choice == 4) {
-                    printf("Enter AT, BT and Priority for P%d: ", i+1);
-                    scanf("%d %d %d", &AT[i], &BT[i], &PR[i]);
-                } else {
-                    printf("Enter AT and BT for P%d: ", i+1);
-                    scanf("%d %d", &AT[i], &BT[i]);
-                }
+        for (int i = 0; i < n; i++) {
+            if (!done[i] && p[i].at <= time && p[i].pr < high) {
+                high = p[i].pr;
+                idx = i;
             }
         }
 
-        switch(choice) {
-            case 1: fcfs(); break;
-            case 2: sjf(); break;
-            case 3: roundRobin(); break;
-            case 4: priority(); break;
-            case 5: printf("Exiting program\n"); break;
-            default: printf("Invalid choice\n");
+        if (idx == -1) {
+            time++;
+            continue;
         }
 
-    } while(choice != 5);
+        gp[gc] = p[idx].pid;
+        gt[gc++] = time;
+
+        time += p[idx].bt;
+        p[idx].ct = time;
+        p[idx].tat = p[idx].ct - p[idx].at;
+        p[idx].wt = p[idx].tat - p[idx].bt;
+
+        done[idx] = 1;
+        completed++;
+    }
+    gt[gc] = time;
+
+    printGantt(gp, gt, gc);
+
+    printf("\nPID AT BT PR CT TAT WT\n");
+    for (int i = 0; i < n; i++)
+        printf("P%d  %d  %d  %d  %d  %d  %d\n",
+               p[i].pid, p[i].at, p[i].bt, p[i].pr,
+               p[i].ct, p[i].tat, p[i].wt);
+}
+
+int main() {
+    int choice, n;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    struct process p[n];
+
+    for (int i = 0; i < n; i++) {
+        p[i].pid = i + 1;
+        printf("Enter AT, BT and Priority for P%d: ", i + 1);
+        scanf("%d %d %d", &p[i].at, &p[i].bt, &p[i].pr);
+    }
+
+    do {
+        printf("\n--- CPU Scheduling Menu ---\n");
+        printf("1. FCFS\n");
+        printf("2. SJF (Non-Preemptive)\n");
+        printf("3. Round Robin\n");
+        printf("4. Priority (Non-Preemptive)\n");
+        printf("5. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: fcfs(p, n); break;
+            case 2: sjf(p, n); break;
+            case 3: roundRobin(p, n); break;
+            case 4: priority(p, n); break;
+            case 5: printf("Exiting...\n"); break;
+            default: printf("Invalid choice\n");
+        }
+    } while (choice != 5);
 
     return 0;
 }
